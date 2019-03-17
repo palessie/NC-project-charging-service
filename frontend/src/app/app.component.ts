a1913a066333fca8ecc63cdbd2dde22adb8c8354
palessie <1869680@gmail.com>
2019-03-17 19:28:35 +0300

    first commit

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'frontend';
+
+  constructor(private  http: HttpClient){
+
+  }
+
+  public clickFunction():void{
+    this.http.get("/api/").subscribe(value => {
+      console.log(value)
+    })
+  }
+}
